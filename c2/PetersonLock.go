50261c0ad481f1e6356c155cc830e24910ceb4e8
Nan Lin <hzlinnan2013@corp.netease.com>
2018-12-05 23:15:35 +0800

    PetersonLock works now, but not as efficient as sync.Mutex ...

--- c2/PetersonLock.go
+++ c2/PetersonLock.go
@@ -1,21 +1,24 @@
 package c2
 
-import "runtime"
+import (
+	"runtime"
+	"sync/atomic"
+)
 
 type PetersonLock struct {
-	flag   [2]bool
-	victim int
+	flag   [2]int32
+	victim int32
 }
 
-func (l *PetersonLock) Lock(i int) {
+func (l *PetersonLock) Lock(i int32) {
 	j := 1 - i
-	l.flag[i] = true
-	l.victim = i
-	for l.flag[j] && l.victim == i {
+	atomic.StoreInt32(&l.flag[i], 1)
+	atomic.StoreInt32(&l.victim, i)
+	for atomic.LoadInt32(&l.flag[j]) == 1 && atomic.LoadInt32(&l.victim) == i {
 		runtime.Gosched()
 	}
 }
 
-func (l *PetersonLock) Unlock(i int) {
-	l.flag[i] = false
+func (l *PetersonLock) Unlock(i int32) {
+	atomic.StoreInt32(&l.flag[i], 0)
 }
--- c2/PetersonLock_test.go
+++ c2/PetersonLock_test.go
@@ -11,21 +11,12 @@ func TestPetersonLock(t *testing.T) {
 	counter := int64(0)
 	var wait sync.WaitGroup
 	const N = 1000000
-	var c1, c2 bool
 	wait.Add(2)
 	go func() {
 		defer wait.Done()
 		for i := 0; i < N; i++ {
 			l.Lock(0)
-			if c2 {
-				println("bad")
-			}
-			c1 = true
-			if c2 {
-				println("bad")
-			}
 			counter = counter + 1
-			c1 = false
 			l.Unlock(0)
 		}
 	}()
@@ -34,15 +25,7 @@ func TestPetersonLock(t *testing.T) {
 		defer wait.Done()
 		for i := 0; i < N; i++ {
 			l.Lock(1)
-			if c1 {
-				println("bad")
-			}
-			c2 = true
-			if c1 {
-				println("bad")
-			}
 			counter = counter + 1
-			c2 = false
 			l.Unlock(1)
 		}
 	}()
@@ -52,6 +35,36 @@ func TestPetersonLock(t *testing.T) {
 	if counter != N*2 {
 		t.Errorf("counter is %d, should be %d", counter, N*2)
 	}
+}
 
-	println(c1, c2)
+func TestMutex(t *testing.T) {
+	var l sync.Mutex
+	counter := int64(0)
+	var wait sync.WaitGroup
+	const N = 1000000
+	wait.Add(2)
+	go func() {
+		defer wait.Done()
+		for i := 0; i < N; i++ {
+			l.Lock()
+			counter = counter + 1
+			l.Unlock()
+		}
+	}()
+
+	go func() {
+		defer wait.Done()
+		for i := 0; i < N; i++ {
+			l.Lock()
+			counter = counter + 1
+			l.Unlock()
+		}
+	}()
+
+	wait.Wait()
+	time.Sleep(time.Second)
+	if counter != N*2 {
+		t.Errorf("counter is %d, should be %d", counter, N*2)
+	}
+
 }
